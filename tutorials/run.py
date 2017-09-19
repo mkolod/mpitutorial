3755e04d9c7218262587d0bfbe58ed64cff4ccc1
Marek Kolodziej <mkolod@gmail.com>
2017-09-19 11:43:35 -0700

    Allow run as root

--- tutorials/run.py
+++ tutorials/run.py
@@ -54,7 +54,7 @@ else:
     mpirun = os.environ.get('MPIRUN', 'mpirun')
     hosts = '' if not os.environ.get('MPI_HOSTS') else '-f {0}'.format(os.environ.get('MPI_HOSTS'))
 
-    sys_call = '{0} -n {1} {2} ./{3}/code/{4}'.format(
+    sys_call = '{0} --allow-run-as-root -n {1} {2} ./{3}/code/{4}'.format(
         mpirun, programs[program_to_run][1], hosts, programs[program_to_run][0], program_to_run)
 
     if len(programs[program_to_run]) > 2:
